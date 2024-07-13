e524db283df84545c4963ab0272eb2d1295b3a48
seda <seda.diriker@gmail.com>
2024-07-13 17:01:49 +0300

    employee added

--- src/components/List.js
+++ src/components/List.js
@@ -2,33 +2,45 @@ import React, { useState } from "react";
 import "./_List.scss";
 import { data } from "../helper/data";
 const List = () => {
-  const [currentPage, setCurrentPage] = useState(1)
-  const workersPerPage = 5
+  const [currentPage, setCurrentPage] = useState(1);
+  const workersPerPage = 5;
 
   const paginatedWorkers = data.slice(
     (currentPage - 1) * workersPerPage,
     workersPerPage * currentPage
-  )
+  );
 
   const nextPage = () => {
-    setCurrentPage(currentPage + 1)
-  }
+    setCurrentPage(currentPage + 1);
+  };
 
   const prevPage = () => {
-    setCurrentPage(currentPage - 1)
-  }
+    setCurrentPage(currentPage - 1);
+  };
   return (
     <div className="list">
-      <div>
+      <div className="title-main">
         <h3 className="title">Employee List</h3>
-        <p></p>
+        <p>
+          {" "}
+          Employess {(workersPerPage * currentPage)- workersPerPage + 1} to {workersPerPage * currentPage}
+        </p>
       </div>
       <div className="employee-list">
         {paginatedWorkers?.map(({ name, age, image, email, id }) => (
           <div className="employee" key={id}>
             <img src={image} alt={name} />
+            <div>
+              <p className="name">{name}</p>
+              <p>{email}</p>
+              <p>{age} years</p>
+            </div>
           </div>
         ))}
+      </div>
+      <div className="btns">
+        <button>Prev</button>
+        <button>Next</button>
       </div>
     </div>
   );
