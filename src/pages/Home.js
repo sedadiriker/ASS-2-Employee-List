5e94611aabaddce4087cb1c0aaf537492011aad3
seda <seda.diriker@gmail.com>
2024-07-13 14:45:46 +0300

    created home

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import List from '../components/List'
+import "./_Home.scss"
+
+const Home = () => {
+  return (
+    <div className="home">
+      <List/>
+    </div>
+  )
+}
+
+export default Home
